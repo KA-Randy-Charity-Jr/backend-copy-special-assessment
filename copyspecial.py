8274135d2a2ad7a0b41e394432b15dbae7f425f9
rakechj <rakechj2@outlook.com>
2020-06-13 20:52:32 -0400

    fixed errors

--- copyspecial.py
+++ copyspecial.py
@@ -18,7 +18,7 @@ import argparse
 
 
 def get_special_paths(dirname):
-    """Given a dirname, returns a list of all its special files."""
+    
     list_files = []
     files = os.listdir(dirname)
     for file in files:
@@ -43,7 +43,7 @@ def copy_to(path, files):
 def zip_to(path, dest_zip):
     # your code here
     paths = list(path)
-    command = "zip -j {} {}".format(dest_zip, ' '.join(paths)) #thanks Doug
+    command = "zip -j {} {}".format(dest_zip, ' '.join(paths))
     print("Command I am going to do: ")
     print(command)
     os.system(command)
@@ -55,20 +55,14 @@ def zip_to(path, dest_zip):
 
 
 def main(args):
-    """Main driver code for copyspecial."""
-    # This snippet will help you get started with the argparse module.
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     parser.add_argument('fromdir', help='src dir to look for local files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
+
     ns = parser.parse_args(args)
-    # TODO: you must write your own code to get the command line args.
-    # Read the docs and examples for the argparse module about how to do this.
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation. If something is wrong (or missing) with
-    # any required args, the general rule is to print a usage message and
-    # exit(1).
+
     all_paths = get_special_paths(ns.fromdir)
     if ns.todir:
         copy_to(ns.todir, all_paths)
@@ -76,9 +70,8 @@ def main(args):
         zip_to(all_paths, ns.tozip)
     if not ns.todir and not ns.tozip:
         print('\n'.join(all_paths))
-        # for file in all_paths:
-        #     print(os.path.abspath(file))
-    # Your code here: Invoke (call) your functions
+        
+    
 
 
 
